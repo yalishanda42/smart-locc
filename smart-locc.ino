9d8bea4e93df3dab5f934ce461d62e939469d6f8
Alexander Ignatov <yalishanda@abv.bg>
2021-04-15 22:38:50 +0300

    Add stubs and nodemcu header

---
+++ smart-locc.ino
@@ -0,0 +1,14 @@
+// Uncomment the following line if you are using NodeMCU
+//#include "nodemcu.h"
+
+//#include <ESP8266WiFi.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Booting up...");
+    Serial.println("Booted up.");
+}
+
+void loop() {
+
+}
