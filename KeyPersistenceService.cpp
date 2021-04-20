64bf78e95c1175715ca6fa4b56835e7b0f39f485
Alexander Ignatov <yalishanda@abv.bg>
2021-04-21 01:09:03 +0300

    Add EEPROM library, implement initguard and key presence check

--- KeyPersistenceService.cpp
+++ KeyPersistenceService.cpp
@@ -1,19 +1,71 @@
 #include "KeyPersistenceService.hpp"
 
-bool KeyPersistenceService::keyIsAuthorized(KeyID) const
-{
-    // TODO
+KeyID::KeyID(unsigned char bytes[KEY_SIZE_BYTES]) {
+    for (unsigned int i = 0; i < KEY_SIZE_BYTES; i++) {
+        this->bytes[i] = bytes[i];
+    }
+}
+
+bool KeyID::operator==(const KeyID& rhs) const {
+    for (unsigned int i = 0; i < KEY_SIZE_BYTES; i++) {
+        if (bytes[i] != rhs.bytes[i]) {
+            return false;
+        }
+    }
     return true;
 }
 
-bool KeyPersistenceService::keyIsAdmin(KeyID) const
-{
-    // TODO
-    return true;
+const char KeyPersistenceService::INITGUARD[INITGUARD_LENGTH + 1] = "key";
+
+KeyPersistenceService::KeyPersistenceService() : numberOfKeys(0) {
+    EEPROM.begin(EEPROM.length());
+
+    // To prevent reading and trusting uninitialized data
+    // we should ensure that the first 3 bytes of EEPROM are always
+    // the same and set by our program.
+    //
+    // The byte after the password should hold the number of keys persisted.
+
+    bool isInitializedProperly = true;
+    for (unsigned int i = 0; i < INITGUARD_LENGTH; i++) {
+        if (EEPROM.read(i) != INITGUARD[i]) {
+            isInitializedProperly = false;
+            break;
+        }
+    }
+
+    if (!isInitializedProperly) {
+        for (unsigned int i = 0; i < INITGUARD_LENGTH; i++) {
+            EEPROM.write(i, INITGUARD[i]);
+        }
+        EEPROM.write(INITGUARD_LENGTH, numberOfKeys);
+    } else {
+        numberOfKeys = EEPROM.read(INITGUARD_LENGTH);
+    }
 }
 
-bool KeyPersistenceService::addKey(KeyID) const
-{
+bool KeyPersistenceService::keyIsAuthorized(KeyID key) const {
+    unsigned int addr = INITGUARD_LENGTH + 1;
+    KeyID currentKey = { 0 };
+    for (unsigned int i = 0; i < numberOfKeys; i++) {
+        EEPROM.get(addr, currentKey);
+
+        if (currentKey == key) {
+            return true;
+        }
+
+        addr += KEY_SIZE_BYTES;
+    }
+
+    return false;
+}
+
+bool KeyPersistenceService::keyIsAdmin(KeyID) const {
+    // TODO
+    return false;
+}
+
+bool KeyPersistenceService::addKey(KeyID) const {
     // TODO
     return true;
 }
--- KeyPersistenceService.hpp
+++ KeyPersistenceService.hpp
@@ -1,12 +1,26 @@
 #ifndef __KEY_PERSISTENCE_SERVICE__HPP__
 #define __KEY_PERSISTENCE_SERVICE__HPP__
 
+#include <EEPROM.h>
+
 #define KEY_SIZE_BYTES 12
-typedef unsigned char KeyID[KEY_SIZE_BYTES];
+struct KeyID {
+    unsigned char bytes[KEY_SIZE_BYTES];
+
+    KeyID(unsigned char[KEY_SIZE_BYTES]);
+    bool operator==(const KeyID &) const;
+};
 
 class KeyPersistenceService
 {
+    static const int INITGUARD_LENGTH = 3;
+    static const char INITGUARD[INITGUARD_LENGTH + 1];
+
+    unsigned char numberOfKeys;
+
 public:
+    KeyPersistenceService();
+
     bool keyIsAuthorized(KeyID) const;
     bool keyIsAdmin(KeyID) const;
     bool addKey(KeyID) const;
